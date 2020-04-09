25e56cf7fca3843952fcc5a06b5d619dc1ace43e
Ortega Sergey <OrtegaSergey@gmail.com>
2020-04-09 19:07:51 +0300

    FirstCommit

---
+++ src/main/java/ru/otrega/spring/mainPackage/Main.java
@@ -0,0 +1,16 @@
+package ru.otrega.spring.mainPackage;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+        Robot robot = (Robot) applicationContext.getBean("Robot");
+
+
+    }
+}
