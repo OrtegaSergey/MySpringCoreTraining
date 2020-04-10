cf169d10681a43fbbc4aad62e886f4facdf3000b
Ortega Sergey <OrtegaSergey@gmail.com>
2020-04-10 12:46:58 +0300

    DI with setter

--- src/main/java/ru/otrega/spring/mainPackage/MusicPlayer.java
+++ src/main/java/ru/otrega/spring/mainPackage/MusicPlayer.java
@@ -2,8 +2,18 @@ package ru.otrega.spring.mainPackage;
 
 public class MusicPlayer {
     Music music;
+    
+    public MusicPlayer(){}
 
     public MusicPlayer(Music music){
+        this.music = music;
+    }
+
+    public Music getMusic() {
+        return music;
+    }
+
+    public void setMusic(Music music) {
         this.music = music;
     }
 
